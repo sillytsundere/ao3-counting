1edac121f4b81cf9ad37447cbae26ca3280b033d
Paige <paigehcarroll@gmail.com>
2025-04-26 21:16:36 -0500

    added some practice functions and moved html file into folder for organizational purposes

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import eel
+from datetime import datetime as dt
+
+@eel.expose
+def hello():
+    print("Hello World!")
+
+eel.init("www")
+eel.start("index.html", block=False)
+
+while True:
+    timestamp = dt.now()
+    eel.addText("The time is now {}".format(timestamp.strftime("%I:%M:%S %p")))
+
+    eel.sleep(1.0)
