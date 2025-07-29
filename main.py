3853560efaf3efa8bf4bc0311abb9d17aecbc2b8
Paige <paigehcarroll@gmail.com>
2025-07-28 22:18:08 -0500

    experimented with functions and objects from api(s), discovered more than one api is installed and affecting functions, will separate into different py files

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import eel
 # from datetime import datetime as dt
+# import AO3
+# api = AO3()
+
+# Use the wendytg/ao3_api library
+# import ao3_api
+# session = ao3_api.Session()
+# work = ao3_api.Work(work_id="52884502", session=session)
+
+# Use the alexwlchan/ao3 library
 import AO3
+# work = ao3.Work(52884502)
+
+# work = api.work(id='52884502')
+
 
 @eel.expose
 def hello():
     print("Hello World!")
 
-url = "https://archiveofourown.org/works/52884502/chapters/139528555"
-workid = AO3.utils.workid_from_url(url)
+url = "https://archiveofourown.org/works/52884502"
+# workid = AO3.utils.workid_from_url(url)
+workid = "52884502"
 print(f"Work ID: {workid}")
 work = AO3.Work(workid)
-print(f"Chapters: {work.nchapters}")
-print(f"Work Chapters?: {work.chapters.work}")
-# title = work[7:-1]
-# print(f"Title: {title}")
-# print(f"chapter 1 title: {work.chapters[0].text}")
-print(len(work.chapters[0].text.split(" ")))
+# print(f"Chapters: {work.nchapters}")
+# print(f"Work Chapters?: {work.chapters.work}")
+title = work.title
+print(f"Title: {title}")
+author = work.author
+print(f"Author: {author}")
+summary = work.summary
+print(f"Summary: {summary}")
+words = work.words
+print(f"Words: {words}")
+# print(f"chapter 1 title: {work.nchapters}")
+data = work.json()
+# print(f"Work: {data}")
 
 
 eel.init("www")
