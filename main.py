96cacdd23f0dd9fc1c9007d67572793f4107045a
Paige <paigehcarroll@gmail.com>
2025-08-18 20:08:26 -0500

    added py files to separate api's

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import eel
 
 # Use the wendytg/ao3_api library
 # import ao3_api
+# print(ao3_api.__file__)
 # session = ao3_api.Session()
 # work = ao3_api.Work(work_id="52884502", session=session)
 
